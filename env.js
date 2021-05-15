81a4601d9267b4710c5338f9c69dac5fdc32a276
YJK <a@conn.cc>
2021-05-15 23:59:23 +0800

    init

---
+++ env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'StartSocks',
+  // 站点描述
+  description: '这里没有你想要的',
+  // API
+  host: 'https://v3.startsocks.com',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'light',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://www.7ed.net/bing/api?cn=true'
+}
